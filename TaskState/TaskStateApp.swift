159202824b64ecf9399a0f8c86b51288700af75c
Seth Faxon <seth.faxon@gmail.com>
2022-09-22 14:46:38 -0700

    Initial Commit

---
+++ TaskState/TaskStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskStateApp.swift
+//  TaskState
+//
+//  Created by Seth Faxon on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
