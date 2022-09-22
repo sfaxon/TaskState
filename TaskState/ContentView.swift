3b08b6613de0a81689d6c0390c121521b9427a94
Seth Faxon <seth.faxon@gmail.com>
2022-09-22 15:05:58 -0700

    Basic setup that shows share sheet content on second viewing
    ContentView.shareItem is set on the Task stack frame. The sheet is
    shown, because `isSharing` is set. But the result of the shareItem
    @State property is still nil inside the sheet presentation.

--- TaskState/ContentView.swift
+++ TaskState/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isSharing = false
+    @State private var shareItem: ShareItem?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                Task {
+                    try? await doTask()
+                }
+            } label: {
+                Text("show sheet")
+            }
+            .sheet(isPresented: $isSharing, content: { shareView })
         }
         .padding()
+    }
+
+    func doTask() async throws {
+        shareItem = try? await ShareItem.build()
+        isSharing = true
+    }
+
+    @ViewBuilder
+    private var shareView: some View {
+        if let share = shareItem {
+            SheetView(shareItem: share)
+        }
+    }
+}
+
+struct SheetView: View {
+    let shareItem: ShareItem
+
+    var body: some View {
+        VStack {
+            Text("it worked!")
+            Text("shareItem.value is \(shareItem.value)")
+        }
+    }
+}
+
+class ShareItem {
+    let value = 1 // no real meaning
+    init() {}
+
+    static func build() async throws -> ShareItem {
+        try await Task.sleep(nanoseconds: 1000)
+        return ShareItem()
     }
 }
 
